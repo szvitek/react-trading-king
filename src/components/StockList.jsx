a30d8021b937757b705bbf458450ae57864f8552
szvitek <gy.szvitek@gmail.com>
2023-04-10 02:27:49 +0100

    feat(StockList): render data in a table

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,9 +1,18 @@
 import { useState, useEffect } from 'react';
+import { BsFillCaretUpFill, BsFillCaretDownFill } from 'react-icons/bs';
 import finnHub from '../apis/finnHub';
 
 const StockList = () => {
-  const [stock, setStock] = useState();
+  const [stock, setStock] = useState([]);
   const [watchList, setWatchList] = useState(['GOOGL', 'MSFT', 'AMZN']);
+
+  const changeColor = (change) => {
+    return change > 0 ? 'success' : 'danger';
+  };
+
+  const renderIcon = (change) => {
+    return change > 0 ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />;
+  };
 
   useEffect(() => {
     let isMounted = true;
@@ -26,6 +35,7 @@ const StockList = () => {
 
         // prevent setStock call if component is NOT mounted
         if (isMounted) {
+          console.log(('set stock', isMounted, data));
           setStock(data);
         }
       } catch (err) {}
@@ -33,12 +43,43 @@ const StockList = () => {
 
     fetchData();
 
-    return () => {
-      isMounted = false;
-    };
+    return () => (isMounted = false);
   }, []);
 
-  return <div>StockList</div>;
+  return (
+    <table className="table hover mt-5">
+      <thead style={{ color: 'rgb(79,89,102)' }}>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Last</th>
+          <th scope="col">Chg</th>
+          <th scope="col">Chg%</th>
+          <th scope="col">High</th>
+          <th scope="col">Low</th>
+          <th scope="col">Open</th>
+          <th scope="col">Pclose</th>
+        </tr>
+      </thead>
+      <tbody>
+        {stock.map((stockData) => (
+          <tr className="table-row" key={stockData.symbol}>
+            <th scope="row">{stockData.symbol}</th>
+            <td>{stockData.data.c}</td>
+            <td className={`text-${changeColor(stockData.data.d)}`}>
+              {stockData.data.d} {renderIcon(stockData.data.d)}
+            </td>
+            <td className={`text-${changeColor(stockData.data.d)}`}>
+              {stockData.data.dp} {renderIcon(stockData.data.d)}
+            </td>
+            <td>{stockData.data.h}</td>
+            <td>{stockData.data.l}</td>
+            <td>{stockData.data.o}</td>
+            <td>{stockData.data.pc}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default StockList;
